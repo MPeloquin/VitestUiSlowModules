6b1b7a8642a792b9f5d479f982728a9a36a4b7af
Maxime Peloquin <mpeloquin@landr.com>
2022-07-05 17:21:06 -0400

    feat: add files

---
+++ src/App.tsx
@@ -0,0 +1,436 @@
+import { useCounter } from './hooks/useCounter';
+import './App.css';
+import { a1 as a1 } from './TestModules/file-1';
+import { a1 as a2 } from './TestModules/file-2';
+import { a1 as a3 } from './TestModules/file-3';
+import { a1 as a4 } from './TestModules/file-4';
+import { a1 as a5 } from './TestModules/file-5';
+import { a1 as a6 } from './TestModules/file-6';
+import { a1 as a7 } from './TestModules/file-7';
+import { a1 as a8 } from './TestModules/file-8';
+import { a1 as a9 } from './TestModules/file-9';
+import { a1 as a10 } from './TestModules/file-10';
+import { a1 as a11 } from './TestModules/file-11';
+import { a1 as a12 } from './TestModules/file-12';
+import { a1 as a13 } from './TestModules/file-13';
+import { a1 as a14 } from './TestModules/file-14';
+import { a1 as a15 } from './TestModules/file-15';
+import { a1 as a16 } from './TestModules/file-16';
+import { a1 as a17 } from './TestModules/file-17';
+import { a1 as a18 } from './TestModules/file-18';
+import { a1 as a19 } from './TestModules/file-19';
+import { a1 as a20 } from './TestModules/file-20';
+import { a1 as a21 } from './TestModules/file-21';
+import { a1 as a22 } from './TestModules/file-22';
+import { a1 as a23 } from './TestModules/file-23';
+import { a1 as a24 } from './TestModules/file-24';
+import { a1 as a25 } from './TestModules/file-25';
+import { a1 as a26 } from './TestModules/file-26';
+import { a1 as a27 } from './TestModules/file-27';
+import { a1 as a28 } from './TestModules/file-28';
+import { a1 as a29 } from './TestModules/file-29';
+import { a1 as a30 } from './TestModules/file-30';
+import { a1 as a31 } from './TestModules/file-31';
+import { a1 as a32 } from './TestModules/file-32';
+import { a1 as a33 } from './TestModules/file-33';
+import { a1 as a34 } from './TestModules/file-34';
+import { a1 as a35 } from './TestModules/file-35';
+import { a1 as a36 } from './TestModules/file-36';
+import { a1 as a37 } from './TestModules/file-37';
+import { a1 as a38 } from './TestModules/file-38';
+import { a1 as a39 } from './TestModules/file-39';
+import { a1 as a40 } from './TestModules/file-40';
+import { a1 as a41 } from './TestModules/file-41';
+import { a1 as a42 } from './TestModules/file-42';
+import { a1 as a43 } from './TestModules/file-43';
+import { a1 as a44 } from './TestModules/file-44';
+import { a1 as a45 } from './TestModules/file-45';
+import { a1 as a46 } from './TestModules/file-46';
+import { a1 as a47 } from './TestModules/file-47';
+import { a1 as a48 } from './TestModules/file-48';
+import { a1 as a49 } from './TestModules/file-49';
+import { a1 as a50 } from './TestModules/file-50';
+import { a1 as a51 } from './TestModules/file-51';
+import { a1 as a52 } from './TestModules/file-52';
+import { a1 as a53 } from './TestModules/file-53';
+import { a1 as a54 } from './TestModules/file-54';
+import { a1 as a55 } from './TestModules/file-55';
+import { a1 as a56 } from './TestModules/file-56';
+import { a1 as a57 } from './TestModules/file-57';
+import { a1 as a58 } from './TestModules/file-58';
+import { a1 as a59 } from './TestModules/file-59';
+import { a1 as a60 } from './TestModules/file-60';
+import { a1 as a61 } from './TestModules/file-61';
+import { a1 as a62 } from './TestModules/file-62';
+import { a1 as a63 } from './TestModules/file-63';
+import { a1 as a64 } from './TestModules/file-64';
+import { a1 as a65 } from './TestModules/file-65';
+import { a1 as a66 } from './TestModules/file-66';
+import { a1 as a67 } from './TestModules/file-67';
+import { a1 as a68 } from './TestModules/file-68';
+import { a1 as a69 } from './TestModules/file-69';
+import { a1 as a70 } from './TestModules/file-70';
+import { a1 as a71 } from './TestModules/file-71';
+import { a1 as a72 } from './TestModules/file-72';
+import { a1 as a73 } from './TestModules/file-73';
+import { a1 as a74 } from './TestModules/file-74';
+import { a1 as a75 } from './TestModules/file-75';
+import { a1 as a76 } from './TestModules/file-76';
+import { a1 as a77 } from './TestModules/file-77';
+import { a1 as a78 } from './TestModules/file-78';
+import { a1 as a79 } from './TestModules/file-79';
+import { a1 as a80 } from './TestModules/file-80';
+import { a1 as a81 } from './TestModules/file-81';
+import { a1 as a82 } from './TestModules/file-82';
+import { a1 as a83 } from './TestModules/file-83';
+import { a1 as a84 } from './TestModules/file-84';
+import { a1 as a85 } from './TestModules/file-85';
+import { a1 as a86 } from './TestModules/file-86';
+import { a1 as a87 } from './TestModules/file-87';
+import { a1 as a88 } from './TestModules/file-88';
+import { a1 as a89 } from './TestModules/file-89';
+import { a1 as a90 } from './TestModules/file-90';
+import { a1 as a91 } from './TestModules/file-91';
+import { a1 as a92 } from './TestModules/file-92';
+import { a1 as a93 } from './TestModules/file-93';
+import { a1 as a94 } from './TestModules/file-94';
+import { a1 as a95 } from './TestModules/file-95';
+import { a1 as a96 } from './TestModules/file-96';
+import { a1 as a97 } from './TestModules/file-97';
+import { a1 as a98 } from './TestModules/file-98';
+import { a1 as a99 } from './TestModules/file-99';
+import { a1 as a100 } from './TestModules/file-100';
+import { a1 as a101 } from './TestModules/file-101';
+import { a1 as a102 } from './TestModules/file-102';
+import { a1 as a103 } from './TestModules/file-103';
+import { a1 as a104 } from './TestModules/file-104';
+import { a1 as a105 } from './TestModules/file-105';
+import { a1 as a106 } from './TestModules/file-106';
+import { a1 as a107 } from './TestModules/file-107';
+import { a1 as a108 } from './TestModules/file-108';
+import { a1 as a109 } from './TestModules/file-109';
+import { a1 as a110 } from './TestModules/file-110';
+import { a1 as a111 } from './TestModules/file-111';
+import { a1 as a112 } from './TestModules/file-112';
+import { a1 as a113 } from './TestModules/file-113';
+import { a1 as a114 } from './TestModules/file-114';
+import { a1 as a115 } from './TestModules/file-115';
+import { a1 as a116 } from './TestModules/file-116';
+import { a1 as a117 } from './TestModules/file-117';
+import { a1 as a118 } from './TestModules/file-118';
+import { a1 as a119 } from './TestModules/file-119';
+import { a1 as a120 } from './TestModules/file-120';
+import { a1 as a121 } from './TestModules/file-121';
+import { a1 as a122 } from './TestModules/file-122';
+import { a1 as a123 } from './TestModules/file-123';
+import { a1 as a124 } from './TestModules/file-124';
+import { a1 as a125 } from './TestModules/file-125';
+import { a1 as a126 } from './TestModules/file-126';
+import { a1 as a127 } from './TestModules/file-127';
+import { a1 as a128 } from './TestModules/file-128';
+import { a1 as a129 } from './TestModules/file-129';
+import { a1 as a130 } from './TestModules/file-130';
+import { a1 as a131 } from './TestModules/file-131';
+import { a1 as a132 } from './TestModules/file-132';
+import { a1 as a133 } from './TestModules/file-133';
+import { a1 as a134 } from './TestModules/file-134';
+import { a1 as a135 } from './TestModules/file-135';
+import { a1 as a136 } from './TestModules/file-136';
+import { a1 as a137 } from './TestModules/file-137';
+import { a1 as a138 } from './TestModules/file-138';
+import { a1 as a139 } from './TestModules/file-139';
+import { a1 as a140 } from './TestModules/file-140';
+import { a1 as a141 } from './TestModules/file-141';
+import { a1 as a142 } from './TestModules/file-142';
+import { a1 as a143 } from './TestModules/file-143';
+import { a1 as a144 } from './TestModules/file-144';
+import { a1 as a145 } from './TestModules/file-145';
+import { a1 as a146 } from './TestModules/file-146';
+import { a1 as a147 } from './TestModules/file-147';
+import { a1 as a148 } from './TestModules/file-148';
+import { a1 as a149 } from './TestModules/file-149';
+import { a1 as a150 } from './TestModules/file-150';
+import { a1 as a151 } from './TestModules/file-151';
+import { a1 as a152 } from './TestModules/file-152';
+import { a1 as a153 } from './TestModules/file-153';
+import { a1 as a154 } from './TestModules/file-154';
+import { a1 as a155 } from './TestModules/file-155';
+import { a1 as a156 } from './TestModules/file-156';
+import { a1 as a157 } from './TestModules/file-157';
+import { a1 as a158 } from './TestModules/file-158';
+import { a1 as a159 } from './TestModules/file-159';
+import { a1 as a160 } from './TestModules/file-160';
+import { a1 as a161 } from './TestModules/file-161';
+import { a1 as a162 } from './TestModules/file-162';
+import { a1 as a163 } from './TestModules/file-163';
+import { a1 as a164 } from './TestModules/file-164';
+import { a1 as a165 } from './TestModules/file-165';
+import { a1 as a166 } from './TestModules/file-166';
+import { a1 as a167 } from './TestModules/file-167';
+import { a1 as a168 } from './TestModules/file-168';
+import { a1 as a169 } from './TestModules/file-169';
+import { a1 as a170 } from './TestModules/file-170';
+import { a1 as a171 } from './TestModules/file-171';
+import { a1 as a172 } from './TestModules/file-172';
+import { a1 as a173 } from './TestModules/file-173';
+import { a1 as a174 } from './TestModules/file-174';
+import { a1 as a175 } from './TestModules/file-175';
+import { a1 as a176 } from './TestModules/file-176';
+import { a1 as a177 } from './TestModules/file-177';
+import { a1 as a178 } from './TestModules/file-178';
+import { a1 as a179 } from './TestModules/file-179';
+import { a1 as a180 } from './TestModules/file-180';
+import { a1 as a181 } from './TestModules/file-181';
+import { a1 as a182 } from './TestModules/file-182';
+import { a1 as a183 } from './TestModules/file-183';
+import { a1 as a184 } from './TestModules/file-184';
+import { a1 as a185 } from './TestModules/file-185';
+import { a1 as a186 } from './TestModules/file-186';
+import { a1 as a187 } from './TestModules/file-187';
+import { a1 as a188 } from './TestModules/file-188';
+import { a1 as a189 } from './TestModules/file-189';
+import { a1 as a190 } from './TestModules/file-190';
+import { a1 as a191 } from './TestModules/file-191';
+import { a1 as a192 } from './TestModules/file-192';
+import { a1 as a193 } from './TestModules/file-193';
+import { a1 as a194 } from './TestModules/file-194';
+import { a1 as a195 } from './TestModules/file-195';
+import { a1 as a196 } from './TestModules/file-196';
+import { a1 as a197 } from './TestModules/file-197';
+import { a1 as a198 } from './TestModules/file-198';
+import { a1 as a199 } from './TestModules/file-199';
+
+function App() {
+    const { count, increment } = useCounter();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello Vite + React!</p>
+                {a1}
+                {a2}
+                {a3}
+                {a4}
+                {a5}
+                {a6}
+                {a7}
+                {a8}
+                {a9}
+                {a10}
+                {a11}
+                {a12}
+                {a13}
+                {a14}
+                {a15}
+                {a16}
+                {a17}
+                {a18}
+                {a19}
+                {a20}
+                {a21}
+                {a22}
+                {a23}
+                {a24}
+                {a25}
+                {a26}
+                {a27}
+                {a28}
+                {a29}
+                {a30}
+                {a31}
+                {a32}
+                {a33}
+                {a34}
+                {a35}
+                {a36}
+                {a37}
+                {a38}
+                {a39}
+                {a40}
+                {a41}
+                {a42}
+                {a43}
+                {a44}
+                {a45}
+                {a46}
+                {a47}
+                {a48}
+                {a49}
+                {a50}
+                {a51}
+                {a52}
+                {a53}
+                {a54}
+                {a55}
+                {a56}
+                {a57}
+                {a58}
+                {a59}
+                {a60}
+                {a61}
+                {a62}
+                {a63}
+                {a64}
+                {a65}
+                {a66}
+                {a67}
+                {a68}
+                {a69}
+                {a70}
+                {a71}
+                {a72}
+                {a73}
+                {a74}
+                {a75}
+                {a76}
+                {a77}
+                {a78}
+                {a79}
+                {a80}
+                {a81}
+                {a82}
+                {a83}
+                {a84}
+                {a85}
+                {a86}
+                {a87}
+                {a88}
+                {a89}
+                {a90}
+                {a91}
+                {a92}
+                {a93}
+                {a94}
+                {a95}
+                {a96}
+                {a97}
+                {a98}
+                {a99}
+                {a100}
+                {a101}
+                {a102}
+                {a103}
+                {a104}
+                {a105}
+                {a106}
+                {a107}
+                {a108}
+                {a109}
+                {a110}
+                {a111}
+                {a112}
+                {a113}
+                {a114}
+                {a115}
+                {a116}
+                {a117}
+                {a118}
+                {a119}
+                {a120}
+                {a121}
+                {a122}
+                {a123}
+                {a124}
+                {a125}
+                {a126}
+                {a127}
+                {a128}
+                {a129}
+                {a130}
+                {a131}
+                {a132}
+                {a133}
+                {a134}
+                {a135}
+                {a136}
+                {a137}
+                {a138}
+                {a139}
+                {a140}
+                {a141}
+                {a142}
+                {a143}
+                {a144}
+                {a145}
+                {a146}
+                {a147}
+                {a148}
+                {a149}
+                {a150}
+                {a151}
+                {a152}
+                {a153}
+                {a154}
+                {a155}
+                {a156}
+                {a157}
+                {a158}
+                {a159}
+                {a160}
+                {a161}
+                {a162}
+                {a163}
+                {a164}
+                {a165}
+                {a166}
+                {a167}
+                {a168}
+                {a169}
+                {a170}
+                {a171}
+                {a172}
+                {a173}
+                {a174}
+                {a175}
+                {a176}
+                {a177}
+                {a178}
+                {a179}
+                {a180}
+                {a181}
+                {a182}
+                {a183}
+                {a184}
+                {a185}
+                {a186}
+                {a187}
+                {a188}
+                {a189}
+                {a190}
+                {a191}
+                {a192}
+                {a193}
+                {a194}
+                {a195}
+                {a196}
+                {a197}
+                {a198}
+                {a199}
+                <p>
+                    <button type="button" onClick={increment}>
+                        count is: {count}
+                    </button>
+                </p>
+                <p>
+                    Edit <code>App.tsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
